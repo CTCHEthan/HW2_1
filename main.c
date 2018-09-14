c51fbdfa4e4813582d94a06e998e82c47669a5f1
CTCHEthan <eh276027@ohio.edu>
2018-09-14 06:43:51 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 9;
+    int y = x + 3;
+    int z = y + x;
+
+
+    printf("%d, %d, %d", x, y, z);
+
+    //printf("Hello world!\n");
+
+    return 0;
+}
